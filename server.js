a1cd1a46a1d9828805c3087827ee28aaf3186545
Josue Fernandez <josue.0795@gmail.com>
2018-11-10 14:50:11 -0600

    Cambios en la maquina virtual, conexion a la Bd local

--- server.js
+++ server.js
@@ -43,6 +43,6 @@ app.use('/api/hashtagMoreLess', hasthagMoreLess);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
